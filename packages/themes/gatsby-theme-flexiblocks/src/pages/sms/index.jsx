dd46e7364f1b0af63d161158b477a6c2ea50ad5d
shahbazmughal <shahbaz.mughal@hostingshouse.com>
2022-01-26 20:06:28 +0500

    SMS Page Data Updated

---
+++ packages/themes/gatsby-theme-flexiblocks/src/pages/sms/index.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { Container } from 'theme-ui'
+import Layout from '@solid-ui-layout/Layout'
+import Seo from '@solid-ui-components/Seo'
+import Divider from '@solid-ui-components/Divider'
+import SubHeader from '@solid-ui-blocks/SubHeader/SubHeader'
+import Header from '@solid-ui-blocks/Sms/HeaderBlock01'
+import Footer from '@solid-ui-blocks/Sms/FooterBlock01'
+import Hero from '@solid-ui-blocks/Sms/HeroBlock01'
+import FeatureTabOne from '@solid-ui-blocks/Sms/FeaturesWithPhotoBlock05'
+import FeatureTabTwo from '@solid-ui-blocks/Sms/FeaturesWithPhotoBlock01'
+import FeatureTabThree from '@solid-ui-blocks/Sms/FeaturesWithPhotoBlock07'
+import Faq from '@solid-ui-blocks/Sms/FaqBlock02'
+import WhyChooseUs from '@solid-ui-blocks/Sms/FeaturesBlock04'
+import { normalizeBlockContentNodes } from '@blocks-helpers'
+import Main from '@solid-ui-blocks/SEO/Main'
+import Base from '@solid-ui-blocks/SEO/Base'
+import SmsSeo from '@solid-ui-blocks/Sms/SmsSeo'
+import Tabs from '@solid-ui-components/Tabs'
+import theme from './_theme'
+import styles from './_styles'
+import "./sms-page.css"
+
+const IndexPage = props => {
+  const { allBlockContent } = props.data
+  const content = normalizeBlockContentNodes(allBlockContent?.nodes)
+
+  return (
+    <Layout theme={theme} {...props}>
+      <Seo title='SMS Marketing and Beyond' description="Create engaging SMS marketing campaigns and get your text read within seconds of delivery. You can connect to your customers of 200 plus destinations." />
+      {/* Blocks */}
+      <div className="sms-page">
+        <div className="subheader-block">
+          <SubHeader content={content['subheader_sms']} menuJustify='space-between' />
+        </div>
+        <Header content={content['header']} />
+        <Divider space='5' />
+        <Divider space='5' />
+        <Hero content={content['hero']} reverse />
+        <Divider space='5' />
+        <WhyChooseUs content={content['why-choose-us']} />
+        <Divider space='4' />
+        <Container variant='wide' sx={styles.tabsContainer}>
+          <Tabs space={4} variant='pill'>
+            <FeatureTabOne content={content['feature-tab-one']} />
+            <FeatureTabTwo content={content['feature-tab-two']} reverse />
+            <FeatureTabThree content={content['feature-tab-three']} />
+          </Tabs>
+        </Container>
+        <Divider space='5' />
+        <Faq content={content['faq']} />
+        <Divider space='5' />
+        <Divider space='5' />
+        <Footer content={content['footer']} />
+
+        <Main />
+        <Base />
+        <SmsSeo />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query smsBlockContent {
+    allBlockContent(filter: { page: { in: ["sms", "shared"] } }) {
+      nodes {
+        ...BlockContent
+      }
+    }
+  }
+`
+export default IndexPage
---
+++ packages/themes/gatsby-theme-flexiblocks/src/pages/sms/sms-features/insights/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { Container } from 'theme-ui'
+import Layout from '@solid-ui-layout/Layout'
+import Seo from '@solid-ui-components/Seo'
+import Divider from '@solid-ui-components/Divider'
+import SubHeader from '@solid-ui-blocks/SubHeader/SubHeader'
+import Header from '@solid-ui-blocks/InnerPage/HeaderBlock01'
+import Footer from '@solid-ui-blocks/InnerPage/FooterBlock01'
+import { normalizeBlockContentNodes } from '@blocks-helpers'
+import Main from '@solid-ui-blocks/SEO/Main'
+import Base from '@solid-ui-blocks/SEO/Base'
+import Hero from '@solid-ui-blocks/InnerPage/HeroBlock02'
+import FeatureLeft from '@solid-ui-blocks/InnerPage/FeatureLeft'
+import FeatureRight from '@solid-ui-blocks/InnerPage/FeatureRight'
+import theme from './_theme'
+import styles from './_styles'
+import './innerpage.css'
+
+const IndexPage = props => {
+  const { allBlockContent } = props.data
+  const content = normalizeBlockContentNodes(allBlockContent?.nodes)
+
+  return (
+    <Layout theme={theme} {...props} xs={styles.body}>
+      <div className="innerpage-main">
+      <Seo title='Delivery Reports for Text Messages | SMS' description="Get delivery reports for the text message campaigns.Learn about success rate and message send failure along with the detailed logs." keywords="sms report,sms delivery report,text message fails,message send failure,delivery report for text messages,message logs" />
+        {/* Blocks */}
+        <div className="subheader-block">
+          <SubHeader content={content['subheader_sms']} menuJustify='space-between' />
+        </div>
+        <Header content={content['header']} />
+        <Divider space='5' />
+        <Divider space='4' />
+        <Container className="innerpage-heroblock" variant='full' sx={styles.heroContainer}>
+          <Hero content={content['hero']} />
+          <Divider space='5' />
+        </Container>
+        <div className="innerpage-section bg-white img-right">
+          <FeatureLeft content={content['features1']} />
+        </div>
+        <div className="innerpage-section bg-light img-left">
+          <FeatureRight content={content['features2']} />
+        </div>
+        <div className="innerpage-section bg-white img-right">
+          <FeatureLeft content={content['features3']} />
+        </div>
+        <Footer content={content['footer']} />
+        <Main />
+        <Base />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query smsFeaturesPage6BlockContent {
+    allBlockContent(
+      filter: { page: { in: ["sms/features/insights", "shared"] } }
+    ) {
+      nodes {
+        ...BlockContent
+      }
+    }
+  }
+`
+export default IndexPage
