5312b7ec4e7eec142d012b844a00a5c9f533f6f5
shahbazmughal <shahbaz.mughal@hostingshouse.com>
2022-01-26 15:13:37 +0500

    Helper Added

--- packages/plugins/gatsby-plugin-alias-imports/gatsby-node.js
+++ packages/plugins/gatsby-plugin-alias-imports/gatsby-node.js
@@ -1,9 +1,8 @@
-exports.onCreateWebpackConfig = helper => {
-    const { stage, actions, getConfig } = helper
-    if (stage === "develop" || stage === 'build-javascript') {
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+    if (stage === 'build-javascript') {
       const config = getConfig()
       const miniCssExtractPlugin = config.plugins.find(
-        plugin => plugin.constructor.name === "MiniCssExtractPlugin"
+        plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
       )
       if (miniCssExtractPlugin) {
         miniCssExtractPlugin.options.ignoreOrder = true
